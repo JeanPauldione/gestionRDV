e3865a6b6d588bc6370035d5a85b8247395daa5c
jeanpauldione <dionepauljean@gmail.com>
2019-10-25 21:11:04 +0000

    page d'accueil medecin

---
+++ medecin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="CSS/Style.css">
+            <link rel="stylesheet" href="CSS/bootstrap.min.css">
+            
+           
+     </head>
+    <body>
+            <div class="jumbotron" style="margin-top:70px;">
+                <h1>Gestion Rendez-Vous</h1>
+            </div>
+            <nav class="navbar navbar-expand-md bg-blue navbar-dark fixed-top navigation">
+                <a class="navbar-brand"><img src="img/RV.jpeg" class="img-fluid image" width=40 height=></a>
+                <a class="navbar-brand lien"  href="medecin.php">MEDECIN</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                        <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link navi" href="ajouterPlaning.php">Ajouter planing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="listePlaning.php">Liste planing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Log out</a>
+                        </li>  
+                        </ul>
+                    </div>  
+                </nav>
+                <br>
+                <img src="CSS/RDV.jpg" class="img-fluid">
+
+    </body>
+</html>
